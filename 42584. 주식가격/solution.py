3b3158b00c6372dba5a3c74b0ee5f2c7780cb49c
이연재 <leeyj0304@gmail.com>
2025-02-03 08:56:41 +0900

    refactor: [week3] 주식가격 조건문 수정

--- 42584. 주식가격/solution.py
+++ 42584. 주식가격/solution.py
@@ -4,10 +4,10 @@ def solution(prices):
         duration = 0
         for j in range(i + 1, len(prices)):
             duration += 1
-            if prices[j] < prices[i] and j == i + 1:
-                duration = 1
-                break
-            elif prices[j] < prices[i]:
+            # if prices[j] < prices[i] and j == i + 1:
+            #     duration = 1
+            #     break
+            if prices[j] < prices[i]:
                 break
         answer.append(duration)
     return answer
