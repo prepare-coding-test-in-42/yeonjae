49d2677bfc6fa4637fd261aeb7817a7610d2d769
이연재 <leeyj0304@gmail.com>
2025-07-25 20:04:31 +0900

    feat: Optimize memory usage by removing curr lists inside the loop

--- 2096. 내려가기/solution.py
+++ 2096. 내려가기/solution.py
@@ -8,24 +8,15 @@ prev_max = board[0][:]
 prev_min = board[0][:]
 
 for i in range(1, N):
-    row = board[i]
+    a0, a1, a2 = prev_max
+    b0, b1, b2 = prev_min
 
-    # 현재 행에 대한 최대, 최소 점수 임시 저장
-    curr_max = [0] * 3
-    curr_min = [0] * 3
+    prev_max[0] = max(a0, a1) + board[i][0]
+    prev_max[1] = max(a0, a1, a2) + board[i][1]
+    prev_max[2] = max(a1, a2) + board[i][2]
 
-    for j in range(3):
-        if j == 0:
-            curr_max[j] = max(prev_max[j], prev_max[j + 1]) + row[j]
-            curr_min[j] = min(prev_min[j], prev_min[j + 1]) + row[j]
-        elif j == 1:
-            curr_max[j] = max(prev_max[j - 1], prev_max[j], prev_max[j + 1]) + row[j]
-            curr_min[j] = min(prev_min[j - 1], prev_min[j], prev_min[j + 1]) + row[j]
-        else:
-            curr_max[j] = max(prev_max[j - 1], prev_max[j]) + row[j]
-            curr_min[j] = min(prev_min[j - 1], prev_min[j]) + row[j]
-
-    prev_max = curr_max
-    prev_min = curr_min
+    prev_min[0] = min(b0, b1) + board[i][0]
+    prev_min[1] = min(b0, b1, b2) + board[i][1]
+    prev_min[2] = min(b1, b2) + board[i][2]
 
 print(max(prev_max), min(prev_min))
